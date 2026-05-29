c35ec3cec8e024d44ca001db376775e6deeeed55
geoffofx <53957912+geoffofx@users.noreply.github.com>
2026-05-29 19:30:37 -0400

    Manually curated holiday list

---
+++ holidays.js
@@ -0,0 +1,12 @@
+// holidays.js
+
+export const customHolidays = [
+    '2026-01-01', // New Year's Day
+    '2026-05-25', // Memorial Day
+    '2026-07-03', // Independence Day (Observed)
+    '2026-09-07', // Labor Day
+    '2026-11-26', // Thanksgiving
+    '2026-11-27', // Black Friday
+    '2026-12-25'  // Christmas
+    // Add as many as you want here over time...
+];
