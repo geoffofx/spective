fdf39465892a8a6240030c0ece1ad18a06cd753d
geoffofx <53957912+geoffofx@users.noreply.github.com>
2026-06-01 19:07:07 -0400

    Update sets.js

--- sets.js
+++ sets.js
@@ -2,15 +2,6 @@
 
 export const customSets = [
     {
-        id: 'project-alpha',
-        dates: [
-            // Week 1: Wed - Fri
-            '2026-06-10', '2026-06-11', '2026-06-12',
-            // Week 2: Mon - Tue (skipping the weekend)
-            '2026-06-15', '2026-06-16'
-        ]
-    },
-    {
         id: 'iretreat2026',
         dates: [
             '2026-06-14', '2026-06-15', '2026-06-16', '2026-06-17', '2026-06-18', '2026-06-19'
