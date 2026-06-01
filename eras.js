c407ff765a1a2c6d633818d9acd3c45fa9fdef66
geoffofx <53957912+geoffofx@users.noreply.github.com>
2026-06-01 03:17:13 -0400

    Add eras.js via upload

---
+++ eras.js
@@ -0,0 +1,15 @@
+// eras.js
+
+export const customEras = [
+    {
+        title: "Summer Break 2026",
+        startDate: "2026-06-15", 
+        endDate: "2026-08-28"
+    },
+    {
+        title: "Living in Chicago",
+        startDate: "2024-03-01",
+        endDate: "2024-09-15"
+    }
+    // Add as many eras as you need here...
+];
