e58ae9780a047229e6442a09e3d1a60d97ae4c53
geoffofx <53957912+geoffofx@users.noreply.github.com>
2026-06-01 17:14:53 -0400

    Create dots.js

---
+++ dots.js
@@ -0,0 +1,60 @@
+// dots.js
+
+export const customDots = [
+    '2022-04-11', // Avril's Birthday
+    '2022-08-19', // Kaiser's Birthday
+    '2022-09-01', // Hazel's Birthday
+    '2022-09-27', // Tonia's Birthday
+    '2022-12-01', // Geoff's Birthday
+    '2022-12-08', // Mom's Birthday
+    '2022-02-27', // Dad's Birthday
+  
+    '2023-04-11', // Avril's Birthday
+    '2023-08-19', // Kaiser's Birthday
+    '2023-09-01', // Hazel's Birthday
+    '2023-09-27', // Tonia's Birthday
+    '2023-12-01', // Geoff's Birthday
+    '2023-12-08', // Mom's Birthday
+    '2023-02-27', // Dad's Birthday
+  
+    '2024-04-11', // Avril's Birthday
+    '2024-08-19', // Kaiser's Birthday
+    '2024-09-01', // Hazel's Birthday
+    '2024-09-27', // Tonia's Birthday
+    '2024-12-01', // Geoff's Birthday
+    '2024-12-08', // Mom's Birthday
+    '2024-02-27', // Dad's Birthday
+  
+    '2025-04-11', // Avril's Birthday
+    '2025-08-19', // Kaiser's Birthday
+    '2025-09-01', // Hazel's Birthday
+    '2025-09-27', // Tonia's Birthday
+    '2025-12-01', // Geoff's Birthday
+    '2025-12-08', // Mom's Birthday
+    '2025-02-27', // Dad's Birthday
+  
+    '2026-04-11', // Avril's Birthday
+    '2026-08-19', // Kaiser's Birthday
+    '2026-09-01', // Hazel's Birthday
+    '2026-09-27', // Tonia's Birthday
+    '2026-12-01', // Geoff's Birthday
+    '2026-12-08', // Mom's Birthday
+    '2026-02-27', // Dad's Birthday
+  
+    '2027-04-11', // Avril's Birthday
+    '2027-08-19', // Kaiser's Birthday
+    '2027-09-01', // Hazel's Birthday
+    '2027-09-27', // Tonia's Birthday
+    '2027-12-01', // Geoff's Birthday
+    '2027-12-08', // Mom's Birthday
+    '2027-02-27', // Dad's Birthday
+  
+    '2028-04-11', // Avril's Birthday
+    '2028-08-19', // Kaiser's Birthday
+    '2028-09-01', // Hazel's Birthday
+    '2028-09-27', // Tonia's Birthday
+    '2028-12-01', // Geoff's Birthday
+    '2028-12-08', // Mom's Birthday
+    '2028-02-27' // Dad's Birthday
+    // Add your specific dates here...
+];
