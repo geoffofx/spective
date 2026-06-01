083df50b312e680c8e71f3dae808c8cd2c3fdc2a
geoffofx <53957912+geoffofx@users.noreply.github.com>
2026-06-01 04:08:55 -0400

    Update eras.js

--- eras.js
+++ eras.js
@@ -2,9 +2,54 @@
 
 export const customEras = [
     {
-        title: "Summer Break 2026",
+        title: "Winter Break 2022",
+        startDate: "2022-12-21", 
+        endDate: "2023-01-02"
+    },
+    {
+        title: "Summer Break 2023",
+        startDate: "2023-06-12", 
+        endDate: "2023-08-27"
+    },
+    {
+        title: "Winter Break 2023",
+        startDate: "2023-12-21", 
+        endDate: "2024-01-02"
+    },
+    {
+        title: "Summer Break 2024",
+        startDate: "2024-06-17", 
+        endDate: "2024-08-25"
+    },
+    {
+        title: "Winter Break 2024",
+        startDate: "2024-12-23", 
+        endDate: "2025-01-02"
+    },
+    {
+        title: "Summer Break 2025",
+        startDate: "2025-06-16", 
+        endDate: "2025-08-24"
+    },
+    {
+        title: "Winter Break 2025",
         startDate: "2026-06-12", 
         endDate: "2026-08-23"
+    },
+    {
+        title: "Summer Break 2026",
+        startDate: "2026-06-15", 
+        endDate: "2026-08-23"
+    },
+    {
+        title: "Winter Break 2026",
+        startDate: "2026-12-23", 
+        endDate: "2027-01-01"
+    },
+    {
+        title: "Summer Break 2027",
+        startDate: "2027-06-14", 
+        endDate: "2027-08-23"
     }
     // Add as many eras as you need here...
 ];
