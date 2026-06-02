c7cde9f520b8af5aa0e32908fbf6269cd8bd8afe
geoffofx <53957912+geoffofx@users.noreply.github.com>
2026-06-02 17:13:57 -0400

    Create chars.js

---
+++ chars.js
@@ -0,0 +1,9 @@
+// chars.js
+
+export const customChars = {
+    '2026-05-15': '✈️',
+    '2026-07-04': '🎆',
+    '2026-10-31': '🎃',
+    '2026-11-26': '🦃'
+    // Add 'YYYY-MM-DD': 'Character' here...
+};
