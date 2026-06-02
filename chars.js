277f1fe3b58eead59b35a456781c8bd800e0f063
geoffofx <53957912+geoffofx@users.noreply.github.com>
2026-06-02 17:57:51 -0400

    Update chars.js

--- chars.js
+++ chars.js
@@ -1,9 +1,60 @@
 // chars.js
 
 export const customChars = {
-    '2026-05-15': '✈️',
-    '2026-07-04': '🎆',
-    '2026-10-31': '🎃',
-    '2026-11-26': '🦃'
+    '2022-04-11': '🗲', // Avril's Birthday
+    '2022-08-19': '✰', // Kaiser's Birthday
+    '2022-09-01': '༄', // Hazel's Birthday
+    '2022-09-27': '∞', // Tonia's Birthday
+    '2022-12-01': '𐦉', // Geoff's Birthday
+    '2022-12-08': '⚬', // Mom's Birthday
+    '2022-02-27': '🞷', // Dad's Birthday
+  
+    '2023-04-11': '🗲', // Avril's Birthday
+    '2023-08-19': '✰', // Kaiser's Birthday
+    '2023-09-01': '༄', // Hazel's Birthday
+    '2023-09-27': '∞', // Tonia's Birthday
+    '2023-12-01': '𐦉', // Geoff's Birthday
+    '2023-12-08': '⚬', // Mom's Birthday
+    '2023-02-27': '🞷', // Dad's Birthday
+  
+    '2024-04-11': '🗲', // Avril's Birthday
+    '2024-08-19': '✰', // Kaiser's Birthday
+    '2024-09-01': '༄', // Hazel's Birthday
+    '2024-09-27': '∞', // Tonia's Birthday
+    '2024-12-01': '𐦉', // Geoff's Birthday
+    '2024-12-08': '⚬', // Mom's Birthday
+    '2024-02-27': '🞷', // Dad's Birthday
+  
+    '2025-04-11': '🗲', // Avril's Birthday
+    '2025-08-19': '✰', // Kaiser's Birthday
+    '2025-09-01': '༄', // Hazel's Birthday
+    '2025-09-27': '∞', // Tonia's Birthday
+    '2025-12-01': '𐦉', // Geoff's Birthday
+    '2025-12-08': '⚬', // Mom's Birthday
+    '2025-02-27': '🞷', // Dad's Birthday
+  
+    '2026-04-11': '🗲', // Avril's Birthday
+    '2026-08-19': '✰', // Kaiser's Birthday
+    '2026-09-01': '༄', // Hazel's Birthday
+    '2026-09-27': '∞', // Tonia's Birthday
+    '2026-12-01': '𐦉', // Geoff's Birthday
+    '2026-12-08': '⚬', // Mom's Birthday
+    '2026-02-27': '🞷', // Dad's Birthday
+  
+    '2027-04-11': '🗲', // Avril's Birthday
+    '2027-08-19': '✰', // Kaiser's Birthday
+    '2027-09-01': '༄', // Hazel's Birthday
+    '2027-09-27': '∞', // Tonia's Birthday
+    '2027-12-01': '𐦉', // Geoff's Birthday
+    '2027-12-08': '⚬', // Mom's Birthday
+    '2027-02-27': '🞷', // Dad's Birthday
+  
+    '2028-04-11': '🗲', // Avril's Birthday
+    '2028-08-19': '✰', // Kaiser's Birthday
+    '2028-09-01': '༄', // Hazel's Birthday
+    '2028-09-27': '∞', // Tonia's Birthday
+    '2028-12-01': '𐦉', // Geoff's Birthday
+    '2028-12-08': '⚬', // Mom's Birthday
+    '2028-02-27': '🞷' // Dad's Birthday
     // Add 'YYYY-MM-DD': 'Character' here...
 };
