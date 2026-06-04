3c47bca1b641b960e3692c5c2fb3903ff5e00117
geoffofx <53957912+geoffofx@users.noreply.github.com>
2026-06-04 15:35:44 -0400

    Update chars.js

--- chars.js
+++ chars.js
@@ -1,73 +1,80 @@
 // chars.js
 
 export const customChars = {
-    '2022-04-11': 'AV', // Avril's Birthday
+    '2022-04-11': 'AV ', // Avril's Birthday
     '2022-08-19': 'KAI', // Kaiser's Birthday
     '2022-09-01': 'HAZ', // Hazel's Birthday
-    '2022-09-27': 'TPX', // Tonia's Birthday
+    '2022-09-27': 'TP♡', // Tonia's Birthday
     '2022-12-01': 'GPX', // Geoff's Birthday
-    '2022-12-08': 'OMI', // Mom's Birthday
+    '2022-12-08': 'Omi', // Mom's Birthday
     '2022-02-27': 'Dad', // Dad's Birthday
+    '2022-06-01': '♡', // Anniversary
     '2022-10-31': '🎃', // Halloween
     '2022-12-25': '🎄', // Christmas
   
     '2023-04-11': 'AV', // Avril's Birthday
     '2023-08-19': 'KAI', // Kaiser's Birthday
     '2023-09-01': 'HAZ', // Hazel's Birthday
-    '2023-09-27': 'TPX', // Tonia's Birthday
+    '2023-09-27': 'TP♡', // Tonia's Birthday
     '2023-12-01': 'GPX', // Geoff's Birthday
-    '2023-12-08': 'OMI', // Mom's Birthday
+    '2023-12-08': 'Omi', // Mom's Birthday
     '2023-02-27': 'Dad', // Dad's Birthday
+    '2023-06-01': '♡', // Anniversary
     '2023-10-31': '🎃', // Halloween
     '2023-12-25': '🎄', // Christmas
   
     '2024-04-11': 'AV', // Avril's Birthday
     '2024-08-19': 'KAI', // Kaiser's Birthday
     '2024-09-01': 'HAZ', // Hazel's Birthday
-    '2024-09-27': 'TPX', // Tonia's Birthday
+    '2024-09-27': 'TP♡', // Tonia's Birthday
     '2024-12-01': 'GPX', // Geoff's Birthday
-    '2024-12-08': 'OMI', // Mom's Birthday
+    '2024-12-08': 'Omi', // Mom's Birthday
     '2024-02-27': 'Dad', // Dad's Birthday
+    '2024-06-01': '♡', // Anniversary
     '2024-10-31': '🎃', // Halloween
     '2024-12-25': '🎄', // Christmas
   
     '2025-04-11': 'AV', // Avril's Birthday
     '2025-08-19': 'KAI', // Kaiser's Birthday
     '2025-09-01': 'HAZ', // Hazel's Birthday
-    '2025-09-27': 'TPX', // Tonia's Birthday
+    '2025-09-27': 'TP♡', // Tonia's Birthday
     '2025-12-01': 'GPX', // Geoff's Birthday
-    '2025-12-08': 'OMI', // Mom's Birthday
+    '2025-12-08': 'Omi', // Mom's Birthday
     '2025-02-27': 'Dad', // Dad's Birthday
+    '2025-06-01': '♡', // Anniversary
     '2025-10-31': '🎃', // Halloween
     '2025-12-25': '🎄', // Christmas
   
     '2026-04-11': 'AV', // Avril's Birthday
     '2026-08-19': 'KAI', // Kaiser's Birthday
     '2026-09-01': 'HAZ', // Hazel's Birthday
-    '2026-09-27': 'TPX', // Tonia's Birthday
+    '2026-09-27': 'TP♡', // Tonia's Birthday
     '2026-12-01': 'GPX', // Geoff's Birthday
-    '2026-12-08': 'OMI', // Mom's Birthday
+    '2026-12-08': 'Omi', // Mom's Birthday
     '2026-02-27': 'Dad', // Dad's Birthday
+    '2026-06-01': '♡', // Anniversary
     '2026-10-31': '🎃', // Halloween
     '2026-12-25': '🎄', // Christmas
   
     '2027-04-11': 'AV', // Avril's Birthday
     '2027-08-19': 'KAI', // Kaiser's Birthday
     '2027-09-01': 'HAZ', // Hazel's Birthday
-    '2027-09-27': 'TPX', // Tonia's Birthday
+    '2027-09-27': 'TP♡', // Tonia's Birthday
     '2027-12-01': 'GPX', // Geoff's Birthday
-    '2027-12-08': 'OMI', // Mom's Birthday
+    '2027-12-08': 'Omi', // Mom's Birthday
     '2027-02-27': 'Dad', // Dad's Birthday
+    '2027-06-01': '♡', // Anniversary
     '2027-10-31': '🎃', // Halloween
     '2027-12-25': '🎄', // Christmas
   
     '2028-04-11': 'AV', // Avril's Birthday
     '2028-08-19': 'KAI', // Kaiser's Birthday
     '2028-09-01': 'HAZ', // Hazel's Birthday
-    '2028-09-27': 'TPX', // Tonia's Birthday
+    '2028-09-27': 'TP♡', // Tonia's Birthday
     '2028-12-01': 'GPX', // Geoff's Birthday
-    '2028-12-08': 'OMI', // Mom's Birthday
+    '2028-12-08': 'Omi', // Mom's Birthday
     '2028-02-27': 'Dad', // Dad's Birthday
+    '2028-06-01': '♡', // Anniversary
     '2028-10-31': '🎃', // Halloween
     '2028-12-25': '🎄' // Christmas
     // Add 'YYYY-MM-DD': 'Character' here...
